e7a8978ffe8553623e72b1938365cd89ff1c57c2
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2021-08-17 07:38:11 +0200

    Project setup with basic linting and import alias.

---
+++ jsconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  compilerOptions: {
+    baseUrl: '.',
+    paths: {
+      '~components': ['./src/components'],
+      '~g': ['./src/gatsby'],
+      '~hooks': ['./src/hooks'],
+      '~src': ['./src'],
+      '~static': ['./static'],
+    },
+  },
+};
