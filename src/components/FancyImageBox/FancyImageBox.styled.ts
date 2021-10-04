8b3c3bae2a30647e9667c5b5ebc1ced6fa731e3e
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2021-10-04 15:26:45 +0200

    Slider

--- src/components/FancyImageBox/FancyImageBox.styled.ts
+++ src/components/FancyImageBox/FancyImageBox.styled.ts
@@ -1,7 +1,81 @@
 import styled from 'styled-components';
 
+// See Thumbs gallery loop from https://swiperjs.com/demos
+// https://codesandbox.io/s/3751v
+
 export const Container = styled.div`
   max-width: 100%;
   width: 700px;
-  background: yellow;
+
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .swiper {
+    width: 100%;
+    height: 300px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .swiper-slide {
+    background-size: cover;
+    background-position: center;
+  }
+
+  .teaser-swiper {
+    height: 80%;
+    width: 100%;
+  }
+
+  .thumbnail-swiper {
+    height: 20%;
+    box-sizing: border-box;
+    padding: 10px 0;
+  }
+
+  .thumbnail-swiper .swiper-slide {
+    width: 25%;
+    height: 100%;
+    opacity: 0.4;
+  }
+
+  .thumbnail-swiper .swiper-slide-thumb-active {
+    opacity: 1;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `;
--- src/components/FancyImageBox/FancyImageBox.tsx
+++ src/components/FancyImageBox/FancyImageBox.tsx
@@ -1,16 +1,16 @@
 import React, { FC, ReactNode, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import SwiperCore, { Navigation, Thumbs } from 'swiper';
 import { Image, Link, Modal } from '~components/index';
 import * as S from './FancyImageBox.styled';
 
 import 'swiper/css/navigation';
+import 'swiper/css';
 import 'swiper/css/thumbs';
 
 // import './styles.css';
 
 // import Swiper core and required modules
-import SwiperCore, { Navigation, Thumbs } from 'swiper';
 
 // install Swiper modules
 SwiperCore.use([Navigation, Thumbs]);
@@ -22,93 +22,39 @@ interface FancyImageBoxProps {
 export const FancyImageBox: FC<FancyImageBoxProps> = ({ teaserImages, thumbnailImages }) => {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
-  const getImageSet = (images) =>
-    images.map((image) => <Image data={image} key={image.id} alt={image?.alt} />);
+  const getSlide = (image) => {
+    return (
+      <SwiperSlide key={image.id}>
+        <Image data={image} alt={image?.alt} />
+      </SwiperSlide>
+    );
+  };
+
+  const getSlides = (images) => images.map((image) => getSlide(image));
+
   return (
     <S.Container>
-      <>
-        <Swiper
-          style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }}
-          loop
-          spaceBetween={10}
-          navigation
-          thumbs={{ swiper: thumbsSwiper }}
-          className="mySwiper2"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-          </SwiperSlide>
-        </Swiper>
-        <Swiper
-          onSwiper={setThumbsSwiper}
-          loop
-          spaceBetween={10}
-          slidesPerView={4}
-          freeMode
-          watchSlidesProgress
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-          </SwiperSlide>
-        </Swiper>
-      </>
-      {/* {getImageSet(teaserImages)}
-      {getImageSet(thumbnailImages)} */}
+      <Swiper
+        style={{ '--swiper-navigation-color': '#fff', '--swiper-pagination-color': '#fff' }}
+        loop
+        spaceBetween={10}
+        navigation
+        thumbs={{ swiper: thumbsSwiper }}
+        className="teaser-swiper"
+      >
+        {getSlides(teaserImages)}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        loop
+        spaceBetween={10}
+        slidesPerView={3}
+        freeMode
+        watchSlidesProgress
+        className="thumbnail-swiper"
+      >
+        {getSlides(thumbnailImages)}
+      </Swiper>
     </S.Container>
   );
 };
