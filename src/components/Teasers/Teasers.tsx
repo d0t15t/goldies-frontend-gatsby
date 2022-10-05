f9ec361d35995a3520c555b0079c1a08a7f5089b
Isaac Bigsby Trogdon <i.trogdon@loom.de>
2022-10-05 12:03:14 +0200

    More products page.

--- src/components/Teasers/Teasers.tsx
+++ src/components/Teasers/Teasers.tsx
@@ -1,15 +1,11 @@
 import React, { FC } from 'react';
 import { useMediaQuery, Grid } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { Swiper, Teaser, TeaserProps, TeasersFancy, ProductTeaser } from '~components/Teasers';
+import { Swiper, Teaser, TeaserProps, TeasersFancy, ProductTeaser, TeaserSimple } from '~components/Teasers';
 import * as S from './Teasers.styled';
 
-export interface TeasersProps {
-  teasers: TeaserProps[];
-}
-
-export const Teasers: FC<TeasersProps> = ({ teasers, listStyle, itemTemplate }) => {
-  const ItemTemplate = itemTemplate;
+export const Teasers = ({ teasers, listStyle, itemTemplate }) => {
+  const ItemTemplate = itemTemplate ?? TeaserSimple;
   const items = teasers.map(item => {
             return (
               <li key={item.id}><ItemTemplate {...item} /></li>
@@ -25,8 +21,8 @@ export const Teasers: FC<TeasersProps> = ({ teasers, listStyle, itemTemplate }) 
     case 'fancy':
       const theme = useTheme();
       const isFancy = useMediaQuery(theme.breakpoints.up('sm'));
-      const Template = isFancy ? TeasersFancy : Swiper;
-      return <Template teasers={teasers} />;
+      const FancyTemplate = isFancy ? TeasersFancy : Swiper;
+      return <FancyTemplate teasers={teasers} />;
     default:
       return (
         <S.TeasersBasic>
